f164459499c80226eb26dc517116c4565e9d92e8
Dharma Krishna Shrestha <sht.dharma@gmail.com>
2023-07-08 15:00:33 +0545

    missing dependency issue fixes in useTransactionsByEmployee.ts

--- src/hooks/useTransactionsByEmployee.ts
+++ src/hooks/useTransactionsByEmployee.ts
@@ -17,7 +17,7 @@ export function useTransactionsByEmployee(): TransactionsByEmployeeResult {
       )
       setTransactionsByEmployee(data)
     },
-    [fetchWithCache]
+    [fetchWithoutCache]
   )
 
   const invalidateData = useCallback(() => {
